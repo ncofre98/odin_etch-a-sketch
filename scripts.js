5ce5e677a83b3b987f9a0270d3fcd491ceb3b9cf
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-01-22 14:43:32 -0600

    Mandatory functionality finished

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
 //UI Elements
 const grid = document.getElementById('grid');
+const gridRange = document.getElementById('gridRange');
+let squaresPerSide = 16;
 
-grid.innerHTML = ('<div class="grid__box"></div>'.repeat(256));
+grid.innerHTML = ('<div class="grid__box"></div>'.repeat(squaresPerSide ** 2));
+
+function updateGridSize(squaresPerSide) {
+    grid.style.setProperty('--squares-per-grid', squaresPerSide);
+    grid.innerHTML = ('<div class="grid__box"></div>'.repeat(squaresPerSide ** 2));
+}
+
+gridRange.addEventListener('input', () => {
+    if (gridRange.value != squaresPerSide) {
+        squaresPerSide = gridRange.value;
+        updateGridSize(squaresPerSide);
+    }
+})
